d701883233ed2256e0cfe6a7d05e0344679fefdc
godwin micheals <godwinmicheals81@gmail.com>
2020-02-04 05:19:58 -0800

    npm clean-install

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,12 +1,12 @@
-// const proxy = require("http-proxy-middleware");
-//
-// module.exports = app => {
-//   // ...
-//   app.use(
-//     "/auth/google",
-//     proxy({
-//       target: "http://localhost:5000",
-//       changeOrigin: true
-//     })
-//   );
-// };
+const proxy = require("http-proxy-middleware");
+
+module.exports = app => {
+  // ...
+  app.use(
+    "/auth/google",
+    proxy({
+      target: "http://localhost:5000",
+      changeOrigin: true
+    })
+  );
+};
